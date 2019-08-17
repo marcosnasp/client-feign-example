36d45f421c460afcee00100f67ab166a0c6ad34f
Marcos Portela <marcosnasp@gmail.com>
2019-08-17 19:17:15 -0300

    Definindo as chamadas para outras operações do serviço marcosnasp/temperatura-converter

--- src/main/java/com/example/feign/client/interfaces/CalculadoraTemperaturaClient.java
+++ src/main/java/com/example/feign/client/interfaces/CalculadoraTemperaturaClient.java
@@ -9,26 +9,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(value = "temperatura-converter-service")
 public interface CalculadoraTemperaturaClient {
 
-	@RequestMapping(method = RequestMethod.GET, 
-			value = "/temperatura/converter/ctof/{tempCelsius}", 
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.GET, value = "/temperatura/converter/ctof/{tempCelsius}", produces = MediaType.APPLICATION_JSON_VALUE)
 	Double celsiusToFarenheit(@PathVariable(value = "tempCelsius") Double tempCelsius);
 
-	/*
-	 * @GetMapping("/ctok/{tempCelsius}") Double celsiusToKelvin(@PathVariable
-	 * Double tempCelsius);
-	 * 
-	 * @GetMapping("/ftoc/{tempFarenheit}") Double farenheitToCelsius(@PathVariable
-	 * Double tempFarenheit);
-	 * 
-	 * @GetMapping("/ftok/{tempFarenheit}") Double farenheitToKelvin(@PathVariable
-	 * Double tempFarenkeit);
-	 * 
-	 * @GetMapping("/ktoc/{tempKelvin}") Double kelvinToCelsius(@PathVariable Double
-	 * tempKelvin);
-	 * 
-	 * @GetMapping("/ktof/{tempKelvin}") Double kelvinToFarenheit(@PathVariable
-	 * Double tempKelvin);
-	 */
+	@RequestMapping(method = RequestMethod.GET, value = "/temperatura/converter/ctok/{tempCelsius}", produces = MediaType.APPLICATION_JSON_VALUE)
+	Double celsiusToKelvin(@PathVariable(value = "tempCelsius") Double tempCelsius);
+
+	@RequestMapping(method = RequestMethod.GET, value = "/temperatura/converter/ftoc/{tempFarenheit}", produces = MediaType.APPLICATION_JSON_VALUE)
+	Double farenheitToCelsius(@PathVariable(value = "tempFarenheit") Double tempFarenheit);
+
+	@RequestMapping(method = RequestMethod.GET, value = "/temperatura/converter/ftok/{tempFarenheit}", produces = MediaType.APPLICATION_JSON_VALUE)
+	Double farenheitToKelvin(@PathVariable(value = "tempFarenheit") Double tempFarenkeit);
+
+	@RequestMapping(method = RequestMethod.GET, value = "/temperatura/converter/ktoc/{tempKelvin}", produces = MediaType.APPLICATION_JSON_VALUE)
+	Double kelvinToCelsius(@PathVariable(value = "tempKelvin") Double tempKelvin);
+
+	@RequestMapping(method = RequestMethod.GET, value = "/temperatura/converter/ktof/{tempKelvin}", produces = MediaType.APPLICATION_JSON_VALUE)
+	Double kelvinToFarenheit(@PathVariable(value = "tempKelvin") Double tempKelvin);
 
 }
